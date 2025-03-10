b12918cd933b5e44f0bed24ce471552e5bee9fd6
mohammed-alhashim <m.alhashim99@hotmail.com>
2025-03-10 14:30:50 +0300

    finished the customer log-in

--- bank_app.py
+++ bank_app.py
@@ -2,8 +2,11 @@ import csv
 
 
 class Account:
-    def __init__(self, account_id, first_name, last_name, password, balance_checking, balance_savings):
-        self.account_id = account_id
+    last_id = 1000
+
+    def __init__(self, first_name, last_name, password, balance_checking, balance_savings):
+        Account.last_id += 1
+        self.account_id = Account.last_id
         self.first_name = first_name
         self.last_name = last_name
         self.password = password
@@ -12,18 +15,67 @@ class Account:
 
     def list_acc(self):
         list_of_acc = [
-            f"{self.account_id};{self.first_name};{self.last_name};{self.password};{self.balance_checking};{self.balance_savings}"]
-        with open('bank.csv', mode='a', newline="") as file:
+            self.account_id, self.first_name, self.last_name, self.password, self.balance_checking,
+            self.balance_savings]
+        with open('bank.csv', mode='a',newline="") as file:
             writer = csv.writer(file, delimiter=';')
             writer.writerow(list_of_acc)
         return list_of_acc
-    
 
-user = Account(1000,"mohammed","ah","pou776",2500,6333)
-print(user.list_acc())
+    def save_to_csv(self):
+        with open('bank.csv', 'a', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow([
+                self.account_id,
+                self.first_name,
+                self.last_name,
+                self.password,
+                self.balance_checking,
+                self.balance_savings
+            ])
+            
+    def login(self, user_name, password_user):
+        if user_name == self.account_id and password_user == self.password:
+            return "successfully log in"
+        else:
+            return "try agin"
 
-# class Withdraw(Account):
-#     pass
+
+class Withdraw(Account):
+    def __init__(self, account):
+        super().__init__(account.first_name, account.last_name, account.password, account.balance_checking,
+                         account.balance_savings)
+
+    def withdraw_money(self, amount):
+        if not self.login(self.account_id, self.password):
+            return "Please log in first"
+
+        if amount > self.balance_checking:
+            return "Insufficient funds"
+
+        self.balance_checking -= amount
+        self.save_to_csv()
+        return f"Withdrawal successful. New balance: {self.balance_checking}"
+
+    def withdraw_from_savings(self, amount):
+        if not self.login(self.account_id, self.password):
+            return "Please log in first"
+        if amount > self.balance_savings:
+            return "Insufficient funds"
+
+        self.balance_savings -= amount
+        self.save_to_csv()
+        return f"Withdrawal successful. New balance: {self.balance_savings}"
+
+
+account1 = Account('mohammed', 'ahmed', 'juagw362', 1000, 10000)
+print(account1.login(account1.account_id, 'juagw362'))
+
+withdraw1 = Withdraw(account1)
+print(withdraw1.withdraw_money(500))
+print(withdraw1.withdraw_from_savings(600))
+print(account1.list_acc())
+
 #
 #
 # class Deposit(Account):
